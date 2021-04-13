9082d7dd278557e40c7f0645414f1fc025786a92
dimasfajar12 <80519340+dimasfajar12@users.noreply.github.com>
2021-04-13 15:22:10 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemograman3368
+{
+    class Karyawan
+    {
+        private int nik;
+        private string nama;
+        private int gajiBulanan;
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public int Nik
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+        public int GajiBulanan
+        {
+            get { return gajiBulanan; }
+            set { gajiBulanan = value; }
+        }
+
+        public Karyawan(string nama, int nik, int gajiBulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            if (gajiBulanan <= 0)
+            {
+                GajiBulanan = 0;
+            }
+            else
+            {
+                GajiBulanan = gajiBulanan;
+            }
+
+        }
+
+
+
+
+        public int PeningkatanGaji(int gajiBulanan)
+        {
+            GajiBulanan = gajiBulanan + ((gajiBulanan / 100) * 10);
+
+            return 0;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ResponsiPemograman3368;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemograman3398
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("Paijo", 190302123, 3000000);
+            Karyawan karyawan2 = new Karyawan("Jono", 190302124, 2000000);
+
+            Console.WriteLine("No Nik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(1 + ". " + karyawan1.Nik + " " + karyawan1.Nama + "\t" + String.Format("{0:n0}", karyawan1.GajiBulanan));
+            Console.WriteLine(2 + ". " + karyawan2.Nik + " " + karyawan2.Nama + "\t" + String.Format("{0:n0}", karyawan2.GajiBulanan));
+
+
+            Console.WriteLine("Alhamdulillah dapat kenaikan gaji 10%");
+            karyawan1.PeningkatanGaji(karyawan1.GajiBulanan);
+            karyawan2.PeningkatanGaji(karyawan2.GajiBulanan);
+
+            Console.WriteLine("No Nik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(1 + ". " + karyawan1.Nik + " " + karyawan1.Nama + "\t" + String.Format("{0:n0}", karyawan1.GajiBulanan));
+            Console.WriteLine(2 + ". " + karyawan2.Nik + " " + karyawan2.Nama + "\t" + String.Format("{0:n0}", karyawan2.GajiBulanan));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
